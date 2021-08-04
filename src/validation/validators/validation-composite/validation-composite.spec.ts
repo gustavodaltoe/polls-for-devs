97232851a5f45381f2d1076f159df84e2769d5d7
gustavodaltoe <gustavodaltoe@hotmail.com>
2021-08-04 13:03:42 -0300

    test: ensure validation composite retuns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 import { FieldValidationSpy } from '../test/mock-field-validation';
 import { ValidationComposite } from './validation-composite';
+import faker from 'faker';
 
 type SutTypes = {
   sut: ValidationComposite;
   fieldValidationsSpy: FieldValidationSpy[];
 };
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('anyField'),
-    new FieldValidationSpy('anyField'),
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName),
   ];
   const sut = new ValidationComposite(fieldValidationsSpy);
   return {
@@ -20,10 +21,19 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut();
-    fieldValidationsSpy[0].error = new Error('first error');
-    fieldValidationsSpy[1].error = new Error('second error');
-    const error = sut.validate('anyField', 'value');
-    expect(error).toBe('first error');
+    const fieldName = faker.database.column();
+    const { sut, fieldValidationsSpy } = makeSut(fieldName);
+    const errorMessage = faker.random.words();
+    fieldValidationsSpy[0].error = new Error(errorMessage);
+    fieldValidationsSpy[1].error = new Error(faker.random.words());
+    const error = sut.validate(fieldName, faker.random.word());
+    expect(error).toBe(errorMessage);
+  });
+
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column();
+    const { sut } = makeSut(fieldName);
+    const error = sut.validate('anyField', faker.random.words());
+    expect(error).toBeFalsy();
   });
 });
