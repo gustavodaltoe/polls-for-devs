5c86982da68f507af938ca69085a269e18091c4f
gustavodaltoe <gustavodaltoe@hotmail.com>
2021-08-04 11:28:26 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,16 @@
+import { FieldValidationSpy } from '../test/mock-field-validation';
+import { ValidationComposite } from './validatior-composite';
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('anyField');
+    const fieldValidationSpy2 = new FieldValidationSpy('anyField');
+    fieldValidationSpy2.error = new Error('some error');
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2,
+    ]);
+    const error = sut.validate('anyField', 'value');
+    expect(error).toBe('some error');
+  });
+});
